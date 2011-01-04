1e4fca942ac9cc45de056a90fa9070c28304347b
Parker Moore <parkrmoore@gmail.com>
2011-01-04 17:56:27 -0500

    tried math, added questions

--- hello.rb
+++ hello.rb
@@ -2,6 +2,21 @@ class World
 	def self.hello
 		puts "Hello!"
 	end
+	def howdy
+	  puts "Howdy, you"
+	end
+	def multiply(hello, you)
+	  puts hello * you
+	end
 end
 
+# ok, i have the static function definition figured out
 World.hello
+# object-specific stuff
+@hello = World.new
+@hello.howdy
+# math?
+@hello.multiply(10000000,3.534243)
+# how do I make a constructor for this thing?
+
+# nice way to format math returns like in java?
